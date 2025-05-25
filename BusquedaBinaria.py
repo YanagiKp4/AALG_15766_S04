da0bd9141b0e7786a3891de407a725342a3cf912
YanagiKp4 <jeancueto.04@gmail.com>
2025-05-25 12:24:08 -0500

    Rel2

--- BusquedaBinaria.py
+++ BusquedaBinaria.py
@@ -1,16 +1,39 @@
-def buslineal(lis, busca):
-    global ops
-    for x in lis:
-        ops+=1
-        if x == busca:
-            return True
-    return False
+def busqueda_binaria_iterativa(lista, objetivo):
+    inicio = 0
+    fin = len(lista) - 1
 
-def busbinaria(lis, busca):
-    pass
+    while inicio <= fin:
+        medio = (inicio + fin) // 2
+        if lista[medio] == objetivo:
+            return medio
+        elif lista[medio] < objetivo:
+            inicio = medio + 1
+        else:
+            fin = medio - 1
+    return -1
 
-ops = 0
-a=[1,3,6,9,4]
-print(buslineal(a,6), ops)
-ops = 0
-print(buslineal(a,8), ops)
+def busqueda_binaria_recursiva(lista, objetivo, inicio=0, fin=None):
+    if fin is None:
+        fin = len(lista) - 1
+
+    if inicio > fin:
+        return -1
+
+    medio = (inicio + fin) // 2
+
+    if lista[medio] == objetivo:
+        return medio
+    elif lista[medio] < objetivo:
+        return busqueda_binaria_recursiva(lista, objetivo, medio + 1, fin)
+    else:
+        return busqueda_binaria_recursiva(lista, objetivo, inicio, medio - 1)
+
+if __name__ == "__main__":
+    datos = [1, 3, 5, 7, 9, 11, 13, 15]
+    objetivo = 7
+
+    resultado_iter = busqueda_binaria_iterativa(datos, objetivo)
+    print(f"Búsqueda Iterativa: El elemento está en la posición {resultado_iter}")
+
+    resultado_rec = busqueda_binaria_recursiva(datos, objetivo)
+    print(f"Búsqueda Recursiva: El elemento está en la posición {resultado_rec}")
